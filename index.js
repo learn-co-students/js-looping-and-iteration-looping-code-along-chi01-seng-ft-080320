6addb3276e610b93688049a2fa5459c99964cb8a
KRStoddard <kimberlynrstoddard@gmail.com>
2020-09-12 12:44:04 -0500

    started changes

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Code your solutions in this file
+//let names = ["Ada", "Brendan", "Ali"]
+//let event = "birthday"
+function writeCards(names, event) {
+    for (let i=0; i < names.length; i++) {
+        console.log(`Thank you, ${names[i]}, for the ${event} gift.`)
+    }
+}
